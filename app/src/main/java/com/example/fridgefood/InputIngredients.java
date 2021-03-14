842771434e14706c82dfa58ed7ebdb5289583c89
yoshikikakehi <yoshikikakehi@gmail.com>
2021-03-13 20:58:24 -0500

    renamed getRecipes to inputIngredients

--- app/src/main/java/com/example/fridgefood/InputIngredients.java
+++ app/src/main/java/com/example/fridgefood/InputIngredients.java
@@ -1,7 +1,6 @@
 package com.example.fridgefood;
 
 import android.Manifest;
-import android.content.Context;
 import android.content.Intent;
 import android.content.pm.PackageManager;
 import android.graphics.Bitmap;
@@ -9,7 +8,6 @@ import android.os.Bundle;
 import android.provider.MediaStore;
 import android.view.View;
 import android.widget.Button;
-import android.graphics.Bitmap;
 import android.widget.ImageButton;
 import android.widget.ImageView;
 
@@ -18,7 +16,7 @@ import androidx.appcompat.app.AppCompatActivity;
 import androidx.core.app.ActivityCompat;
 import androidx.core.content.ContextCompat;
 
-public class GetRecipes extends AppCompatActivity {
+public class InputIngredients extends AppCompatActivity {
     private Button backButton;
     private Button getRecipesButton;
     private ImageButton captureCamera;
@@ -47,9 +45,9 @@ public class GetRecipes extends AppCompatActivity {
             }
         });
 
-        if (ContextCompat.checkSelfPermission(GetRecipes.this,
+        if (ContextCompat.checkSelfPermission(InputIngredients.this,
                 Manifest.permission.CAMERA) != PackageManager.PERMISSION_GRANTED) {
-            ActivityCompat.requestPermissions(GetRecipes.this,
+            ActivityCompat.requestPermissions(InputIngredients.this,
                     new String[]{
                             Manifest.permission.CAMERA
                     }, 100);
--- app/src/main/java/com/example/fridgefood/MainActivity.java
+++ app/src/main/java/com/example/fridgefood/MainActivity.java
@@ -18,7 +18,7 @@ public class MainActivity extends AppCompatActivity {
         button.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                Intent page = new Intent(v.getContext(), GetRecipes.class);
+                Intent page = new Intent(v.getContext(), InputIngredients.class);
                 startActivity(page);
             }
         });
